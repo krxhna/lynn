41cdf60601ac4056e8fc63a921a2f8ea9718c1f3
krxhna <m.krishnakanth1999@gmail.com>
2021-10-09 06:51:36 +0800

    bmi

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -1,2 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+class bmi extends StatefulWidget {
+  // bmi({Key? key}) : super(key: key);
+
+  @override
+  _bmiState createState() => _bmiState();
+}
+
+class _bmiState extends State<bmi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("bmi"),
+      ),
+    );
+  }
+}
